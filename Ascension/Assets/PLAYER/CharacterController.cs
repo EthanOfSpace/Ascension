92099e872e467db26a3e2045dc5a2e519b306164
EthanOfSpace <testkit06@gmail.com>
2021-10-04 21:01:45 -0400

    Removing Code From character controller

--- Ascension/Assets/PLAYER/CharacterController.cs
+++ Ascension/Assets/PLAYER/CharacterController.cs
@@ -6,17 +6,21 @@ public class CharacterController : MonoBehaviour
 {
 
 
-    public Texture2D cursorImage;
+    public Texture2D mousecursorImage;
 
-    private void Update()
+    private void FixedUpdate()
     {
-        Cursor.SetCursor(cursorImage, Vector2.zero, CursorMode.ForceSoftware);
-
+        Cursor.SetCursor(mousecursorImage, Vector2.zero, CursorMode.ForceSoftware);
 
 
     }
 
+    // This character controller will have its functinality split into states
 
+    // the current state will be controlled by a state machine
+
+
+  
 
 
 }
