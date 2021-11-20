be1830f2a9a6d06e34bc5a40c6156766d30a1fc1
EthanOfSpace <testkit06@gmail.com>
2021-11-20 02:45:25 -0500

    Making comments in the Code

--- Ascension/Assets/PLAYER/CharacterController.cs
+++ Ascension/Assets/PLAYER/CharacterController.cs
@@ -15,9 +15,17 @@ public class CharacterController : MonoBehaviour
 
     }
 
-    // This character controller will have its functinality split into states
+    // The character controller will have its functinality split into states
 
-    // the current state will be controlled by a state machine
+    // The current state will be controlled by a state machine
+
+    // If COMPUTER GAME DEVELOPMENT is the INTERSECTION of ART || TECHNOLOGY || DESIGN
+    
+    // I need to consider how to translate my GAME DESIGN DOCUMENTS into GAME PROGRAMMING DOCUMENTS 
+
+    // The purpose of these GAME PROGRAMMING DOCUMENTS is so that when it comes time for me or someone else to IMPLEMENT the GAME DESIGN
+
+    // They have a GUIDE that lets them know what the REQUIREMENTS of the GAME DESIGN entail, and a RECCOMENDED way of IMPLEMENTING the DESIGN
 
 
   
