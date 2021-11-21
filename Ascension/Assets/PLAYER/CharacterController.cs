07185c0e040b5fb37f8348de6368e81d92c0af2f
EthanOfSpace <testkit06@gmail.com>
2021-11-20 20:34:53 -0500

    Update CharacterController.cs

--- Ascension/Assets/PLAYER/CharacterController.cs
+++ Ascension/Assets/PLAYER/CharacterController.cs
@@ -21,14 +21,18 @@ public class CharacterController : MonoBehaviour
 
     // If COMPUTER GAME DEVELOPMENT is the INTERSECTION of ART || TECHNOLOGY || DESIGN
     
-    // I need to consider how to translate my GAME DESIGN DOCUMENTS into GAME PROGRAMMING DOCUMENTS 
+    // I need to consider how to translate my GAME DESIGN DOCUMENTS into GAME PROGRAMMING DOCUMENTS  
 
     // The purpose of these GAME PROGRAMMING DOCUMENTS is so that when it comes time for me or someone else to IMPLEMENT the GAME DESIGN
 
-    // They have a GUIDE that lets them know what the REQUIREMENTS of the GAME DESIGN entail, and a RECCOMENDED way of IMPLEMENTING the DESIGN
+    // Becuase the game design document is built for the games design, from a programming perspective, it has a lot of clutter and unecasary content to sift through 
+    
+    // The programming document is to remove this filler
 
+    // They have a GUIDE that lets them know what the REQUIREMENTS of the GAME DESIGN entail, and a RECOMMENDED way of IMPLEMENTING the DESIGN
 
+    // Becuase of the constantly evolving nature of iterative game design, game code, above all us, should be flexible to change and open to extension.
   
-
+    
 
 }
