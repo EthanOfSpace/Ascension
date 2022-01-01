dcc7150ba3298e35da01bd359252d9bfaa554807
EthanOfSpace <testkit06@gmail.com>
2022-01-01 10:38:47 -0500

    some new changes

---
+++ Ascension/Assets/CHARACTERS/Character.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour 
+{
+
+    // OBJECT ORIENTED TECHNIQUE: INHERITANCE
+
+    // This is the SUPERCLASS for all characters in the game to inherit from
+
+    // It comes with common attributes and functionality that characters can utilize
+
+    // The PLAYER, the OPPONENTS, the FRIENDLY NPC's, everyone will inherit from this SUPERCLASS
+
+    [SerializeField]
+    public virtual int health { get; set; }
+
+
+    public virtual int getHealth() 
+    {
+        return health;
+    }
+
+    public virtual void setHealth(int healthAmount) 
+    {
+        health = healthAmount;
+    }
+
+    // Fixed Update is called independant of the games framerate
+    public virtual void FixedUpdate() 
+    {
+    
+    
+    }
+
+
+}
---
+++ Ascension/Assets/CHARACTERS/PLAYER/CharacterController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : Character
+{
+
+    public Texture2D cursorImage;
+
+
+    public void Awake()
+    {
+        setHealth(100);
+    }
+
+    public override void FixedUpdate()
+    {
+        MovePlayerReticle(cursorImage);
+    }
+
+
+    void MovePlayerReticle(Texture2D cursorImage) 
+    {
+        Cursor.SetCursor(cursorImage, Vector2.zero, CursorMode.ForceSoftware);
+    }
+    
+    public float zAxisSpeed = 5.0f;
+    public float xAxisSpeed = 10.0f;
+
+    void Update()
+    {
+        // Get the horizontal and vertical axis.
+
+        float zAxis = Input.GetAxis("Vertical") * zAxisSpeed;
+        float xAxis = Input.GetAxis("Horizontal") * xAxisSpeed;
+
+        // Make it move independant of frame rate
+
+        zAxis *= Time.deltaTime;
+        xAxis *= Time.deltaTime;
+
+        // translate this object along the z-axis and x-axis
+        transform.Translate(xAxis, 0, zAxis);
+
+    }
+
+    // The CHARACTER CONTROLLER will have its functinality split into states
+
+    // The current state will be controlled by a state machine
+
+}
